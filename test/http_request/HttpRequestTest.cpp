721dd89e7a10f7b73eebf720d6f114369fdda5d4
kanghyki <kanghyki@gmail.com>
2023-04-24 19:03:09 +0900

    test: httprequest

---
+++ test/http_request/HttpRequestTest.cpp
@@ -0,0 +1,29 @@
+#include "./HttpRequestTest.hpp"
+
+void HttpRequestTest::test() {
+  std::string header = "\
+GET /html/index.html HTTP/1.1\r\n\
+Host: localhost:13744\r\n\
+Connection: keep-alive\r\n\
+Upgrade-Insecure-Requests: 1\r\n\
+Content-Length: 123\r\n\
+Sec-Fetch-Site: none\r\n\
+Sec-Fetch-Mode: navigate\r\n\
+Sec-Fetch-User: ?1\r\n\
+Sec-Fetch-Dest: document\r\n\
+Accept-Encoding: gzip, deflate, br\r\n\
+Accept-Language: en-US,en;q=0.9";
+  HttpRequest hr;
+  
+  try {
+    hr.parseHeader(header);
+  } catch (HttpStatus s) {
+    std::cout << s << std::endl;
+  }
+
+  std::string expect = "";
+  AssertEqual(expect, hr.getBody());
+
+  std::string expect1 = "123";
+  AssertEqual(expect1, hr.getField("Content-Length"));
+}
---
+++ test/http_request/HttpRequestTest.hpp
@@ -0,0 +1,13 @@
+#ifndef HTTP_REQUEST_TEST_HPP
+# define HTTP_REQUEST_TEST_HPP
+
+# include "../test_lib/kunit.hpp"
+# include "../../src/http/HttpRequest.hpp"
+
+class HttpRequestTest {
+  public:
+    static void test();
+  private:
+};
+
+#endif
