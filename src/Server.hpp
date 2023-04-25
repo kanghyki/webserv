086af183a8e6807523a0581813739ad7b634890f
sinryuji <siler2474@gmail.com>
2023-04-25 16:22:47 +0900

    get clean up

--- src/Server.hpp
+++ src/Server.hpp
@@ -30,12 +30,28 @@ class Server {
     static const int BUF_SIZE = 1024;
     static const int MANAGE_FD_MAX = 1024;
     static const int TIMEOUT_MAX = 5;
-    static const int HEADER_NOT_RECV = -1;
-    static const int HEADER_RECV = 0;
+//    static const int HEADER_NOT_RECV = -1;
+//    static const int HEADER_RECV = 0;
 
-    std::vector<std::string> data;
-    std::vector<int> contentLengths;
-    std::vector<size_t> headerPos;
+    struct received {
+      std::string data;
+      int         contentLength;
+      size_t      headerPos;
+      int         status;
+    };
+
+    enum recvStatus {
+      HEADER_NOT_RECV,
+      HEADER_RECV,
+      BODY_RECV
+    };
+
+    std::vector<struct received> recvTable;
+
+//    std::vector<std::string> data;
+//    std::vector<int> contentLengths;
+//    std::vector<size_t> headerPos;
+
     const std::string host;
     const int port;
     int servFd;
@@ -67,15 +83,22 @@ class Server {
     const std::string getData(int fd) const;
     const int         getContentLength(int fd) const;
     const size_t      getHeaderPos(int fd) const;
+    const int         getStatus(int fd) const;
 
     void              addData(int fd, const std::string& data);
     void              setContentLength(int fd, int len);
     void              setHeaderPos(int fd, size_t pos);
+    void              setStatus(int fd, recvStatus status);
 
     void              clearData(int fd);
     void              clearContentLength(int fd);
     void              clearHeaderPos(int fd);
     void              clearReceived(int fd);
+    void              clearStatus(int fd);
+
+    void              recvHeader(int fd);
+    int               parseContentLength(int fd, size_t start);
+    bool              bodyRecvDone(int fd);
 
     std::map<int, time_t> timeout;
     const std::map<int, time_t>& getTimeRecord() const;
