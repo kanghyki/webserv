dc95ba8921447e9e818c2e76d3a89eb0af8346f8
kanghyki <kanghyki@gmail.com>
2023-05-05 13:19:13 +0900

    feat: argument error msg

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,29 @@
 
 
 int main(int argc, char **argv) {
+  std::string config_file;
+
   std::cout << TEAM_MARK;
 
-  if (argc != 2) {
-    log::error << "Required argument. Usage: \"" << argv[0] << " [config_file]\"" << log::endl;
-    return EXIT_FAILURE;
+  if (argc < 2) {
+    config_file = "default.conf";
+    log::warning << "The argument is not entered" << log::endl;
   }
+  else {
+    config_file = argv[1];
+    if (argc > 2)
+      log::warning << "Arguments are ignored after one argument" << log::endl;
+  }
+  log::info << "The config file is set to [" << config_file << "]" << log::endl;
 
   try {
     ConfigParser parser;
-    Config conf = parser.parse(argv[1]);
+    Config conf = parser.parse(config_file);
     log::info << "Config parsing ok" << log::endl;
+
     Server server(conf);
     log::info << "Server setup done" << log::endl;
+
     server.run();
   } catch (std::exception &e) {
     log::error << e.what() << log::endl;
