5146b992b689f9a9d406b5d053674a91da8c4a80
kanghyki <kanghyki@gmail.com>
2023-05-05 12:09:53 +0900

    refactor: add main.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,24 @@
-#include <iostream>
-#include "./Server.hpp"
-#include "./config/parser/ConfigParser.hpp"
-#include "./config/Config.hpp"
-#include "./Logger.hpp"
+#include "./main.hpp"
 
-const std::string TEAM_MARK = "\n\
-  ███    ██  ██████  ██ ███    ██ ██   ██     ███    ███ ██ ███    ██ ██    ██ ███████\n\
-  ████   ██ ██       ██ ████   ██  ██ ██      ████  ████ ██ ████   ██ ██    ██ ██     \n\
-  ██ ██  ██ ██   ███ ██ ██ ██  ██   ███       ██ ████ ██ ██ ██ ██  ██ ██    ██ ███████\n\
-  ██  ██ ██ ██    ██ ██ ██  ██ ██  ██ ██      ██  ██  ██ ██ ██  ██ ██ ██    ██      ██\n\
-  ██   ████  ██████  ██ ██   ████ ██   ██     ██      ██ ██ ██   ████  ██████  ███████\n\n";
 
 int main(int argc, char **argv) {
   std::cout << TEAM_MARK;
+
   if (argc != 2) {
-    log::error << "argument";
+    log::error << "Required argument. Usage: \"" << argv[0] << " [config_file]\"" << log::endl;
     return EXIT_FAILURE;
   }
 
   try {
-    ConfigParser p;
-    Config a = p.parse(argv[1]);
+    ConfigParser parser;
+    Config conf = parser.parse(argv[1]);
     log::info << "Config parsing ok" << log::endl;
-    Server server(a);
+    Server server(conf);
     log::info << "Server setup done" << log::endl;
     server.run();
   } catch (std::exception &e) {
     log::error << e.what() << log::endl;
+    return EXIT_FAILURE;
   }
 
   return EXIT_SUCCESS;
