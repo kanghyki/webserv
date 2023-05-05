293691bcfa428fda19a4ebd067c5f584357b6326
kanghyki <kanghyki@gmail.com>
2023-05-05 12:15:15 +0900

    test: remove

--- test/http_request/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "../test_lib/kunit.hpp"
-#include "./HttpRequestTest.hpp"
-
-int main(void) {
-  Run("test", HttpRequestTest::test);
-  Run("test1", HttpRequestTest::test1);
-}
